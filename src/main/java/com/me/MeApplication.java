1ad2ebb5ec48eb15d9de64630780ede82a2eed54
dgiac85 <dgiac85@gmail.com>
2016-06-26 23:48:43 +0200

    Formatted MeApplication

--- src/main/java/com/me/MeApplication.java
+++ src/main/java/com/me/MeApplication.java
@@ -21,7 +21,7 @@ public class MeApplication {
 	    return model;
 	  }
 
-	public static void main(String[] args) {
+	  public static void main(String[] args) {
 		SpringApplication.run(MeApplication.class, args);
-	}
+	  }
 }
