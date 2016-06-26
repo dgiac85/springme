76fecf790a8711cee2ae5acbe77ed364e43c773d
dgiac85 <dgiac85@gmail.com>
2016-06-26 23:40:14 +0200

    home and login page

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,7 +1,28 @@
-angular.module('hello', [])
+//angular.module('hello', [])
   
-.controller('home', function($scope, $http) {
-	  $http.get('/resource/').success(function(data) {
-		    $scope.greeting = data;
-		  })
+//.controller('home', function($scope, $http) {
+//	  $http.get('/resource/').success(function(data) {
+//		    $scope.greeting = data;
+//		  })
+//})
+
+angular.module('hello', [ 'ngRoute' ])
+.config(function($routeProvider, $httpProvider) {
+
+	$routeProvider.when('/', {
+		templateUrl : 'home.html',
+		controller : 'home'
+	}).when('/login', {
+		templateUrl : 'login.html',
+		controller : 'navigation'
+	}).otherwise('/');
+
+  $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
 })
+.controller('home', function($scope, $http) {
+  $http.get('/resource/').success(function(data) {
+    $scope.greeting = data;
+  })
+})
+.controller('navigation', function() {});
